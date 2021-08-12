121af9a2fe57a2f1bcd070468cec015d718e0acf
Kevin Wekesa <kevinwalk25@gmail.com>
2021-08-12 03:57:11 +0300

    Fixed a bug on the configuration file

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 // DB Params
 define("DB_HOST","localhost");
 define("DB_USER","root");
-define("DB_PASS","1442");
+define("DB_PASS","");
 define("DB_NAME","catalogue");
 
 define ("SITE_TITLE", "Hardware Catalogue ");
